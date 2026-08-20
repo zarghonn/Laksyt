4f3c29e27a9aa36cfea5a677a7e59dd1c6d26aec
Zarghona <zarghonn@metropolia.fi>
2026-08-20 15:02:26 +0300

    2.3

---
+++ teh2.3.py
@@ -0,0 +1,8 @@
+korkeus = float(input("mikä on suorakulmion kanta ja korkeus?"))
+kanta = float(input("mikä on suorakaiteen kanta"))
+
+A = kanta * korkeus
+piiri = kanta + korkeus + kanta + korkeus
+
+print(f"pinta ala on:", A)
+print(f"kanta ja korkeus on:", piiri)
